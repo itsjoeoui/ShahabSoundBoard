a84a544c53dce2c19b6593c29172b9e0c6a72ae7
Hristo Sandev <sandevh@gmail.com>
2021-04-18 20:43:35 -0400

    init commit

--- App.js
+++ App.js
@@ -1,14 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Button, Text, TouchableOpacity, View, Image } from 'react-native';
+import { Audio } from 'expo-av';
 
 export default function App() {
+
+  
+  const [sound, setSound] = React.useState();
+
+  async function playSound() {
+    const { sound } = await Audio.Sound.createAsync(
+       require('./assets/sounds/KillYou.mp3')
+    );
+    setSound(sound);
+
+    console.log('Playing Sound');
+    await sound.playAsync(); }
+
+  React.useEffect(() => {
+    return sound
+      ? () => {
+          console.log('Unloading Sound');
+          sound.unloadAsync(); }
+      : undefined;
+  }, [sound]);
+
   return (
+    
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Shahab time:</Text>
+      <Image source={require("./assets/MathJail.png")}/>
+      <Button title="Shahab" onPress={playSound}/>
       <StatusBar style="auto" />
     </View>
+
   );
+  
 }
 
 const styles = StyleSheet.create({
